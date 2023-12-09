fb33cbf075d15c7c925a0a08a96a312a48038efe
Eduardo Gwiazda <eduardo.gwiazda@telefonica.com>
2023-12-08 21:28:26 -0300

    cambio el nobmre para ver si lo encuenra

---
+++ src/router/shopRouters.js
@@ -0,0 +1,17 @@
+// shopRoutes.js
+const express = require('express');
+const router = express.Router();
+const shopController = require('../controllers/shopController.js');
+
+// shopController.initializeShop();    
+router.get('/', shopController.getShop);
+
+router.get('/item/:id', shopController.getItem);
+
+router.post('/item/:id/add', shopController.addItemToShop);
+
+router.get('/cart', shopController.getShoppingCart);
+
+router.post('/cart', shopController.updateShoppingCart);
+
+module.exports = router ;
